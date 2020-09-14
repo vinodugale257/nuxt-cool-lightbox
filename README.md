bcaea6192fc20dd4cf85b47f83825fb8619cb129
Vinod Ugale <vinod@astutertech.com>
2020-09-14 16:43:59 +0530

    Add initial commit for nuxt-lightbox.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# nuxt-cool-lightbox
-Integrate vue-cool-lightbox package inside nuxt project.
+# Nuxt-cool-lightbox
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/Gallery.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <CoolLightBox
+      :items="items"
+      :index="index"
+      :effect="'fade'"
+      @close="index = null"
+    >
+    </CoolLightBox>
+
+    <div class="images-wrapper">
+      <div
+        v-for="(image, imageIndex) in items"
+        :key="imageIndex"
+        class="image"
+        :style="{ backgroundImage: 'url(' + image + ')' }"
+        @click="index = imageIndex"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        'https://cosmos-images2.imgix.net/file/spina/photo/20565/191010_nature.jpg?ixlib=rails-2.1.4&auto=format&ch=Width%2CDPR&fit=max&w=835',
+        'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nature-quotes-1557340276.jpg?crop=0.666xw:1.00xh;0.168xw,0&resize=640:*',
+      ],
+      index: null,
+    }
+  },
+}
+</script>
